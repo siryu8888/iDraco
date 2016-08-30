58bae4636687879fe9c06e125243814cb29bd647
siryu8888 <siryu8888@gmail.com>
2016-08-30 08:21:37 +0700

    Add Navigation Bar Title

---
+++ iDracoMobile/CustomNavigationController.swift
@@ -0,0 +1,35 @@
+//
+//  CustomNavigationController.swift
+//  iDracoMobile
+//
+//  Created by Billy Chen on 8/30/16.
+//  Copyright © 2016 iTech Ultimate. All rights reserved.
+//
+
+import UIKit
+
+class CustomNavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationBar.topItem!.title = "iDraco POS"
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
